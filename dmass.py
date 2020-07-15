68fe4236e4905f7653059a2b7131a7a0f5b1e5b0
JKSHT <68342857+JKSHT@users.noreply.github.com>
2020-07-15 21:40:03 +0530

    Update dmass.py

--- dmass.py
+++ dmass.py
@@ -17,7 +17,7 @@ async def on_ready():
     print('Logged in as '+client.user.name+' (ID:'+client.user.id+') | Connected to '+str(len(client.servers))+' servers | Connected to '+str(len(set(client.get_all_members())))+' users')
     print('--------')
     print('--------')
-    print('CREATED AND HOSTED BY SUBHANKAR DAS')
+    print('CREATED AND HOSTED BY joker')
 
 @client.command(pass_context = True)
 @commands.has_permissions(kick_members=True)     
@@ -44,4 +44,4 @@ async def send(ctx, *, content: str):
                 await client.say("DM can't Sent To : {} :x: ".format(member))
 
 
-client.run("YOUR BOT'S TOKEN HERE")                
+client.run("NzMwMzk3MzAxNDk4OTcwMTY0.Xw8prA.4AzPCAHDUR1X-gWlWI8OP4mlIOk")                
